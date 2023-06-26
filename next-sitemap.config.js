d77383cb9bba006727624a06c51ae3840f0fde3d
kFuQ <kfuq@kfuq.net>
2023-06-25 17:08:55 -0700

    sitemap bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,7 +8,7 @@ module.exports = {
     exclude: ['/sitemap.xml', '/api'],
     transform: async (config, path) => {
         return {
-            loc: path,
+            loc: `https://${path}`,
             changefreq: config.changefreq,
             priority: config.priority,
             lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
