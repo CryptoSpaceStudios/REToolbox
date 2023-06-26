48f474cac50568ebe516897a00677f4799fd924f
kFuQ <kfuq@kfuq.net>
2023-06-26 14:03:11 -0700

    adsense

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
 import "styles/style.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAdSense } from "nextjs-google-adsense";
 
 const App = ({ Component, pageProps }) => {
   // default theme setup
@@ -54,6 +55,7 @@ const App = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
       </Head>
+      <GoogleAdSense publisherId="" />
       <Component {...pageProps} />
       <Analytics />
     </>
