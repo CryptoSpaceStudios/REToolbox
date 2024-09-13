750c7f5e899a6db74a972efdafb30cd05e572d26
kFuQ <kfuq@kfuq.net>
2024-09-12 20:35:11 -0700

    .

--- next.config.js
+++ next.config.js
@@ -16,18 +16,5 @@ module.exports = withPlugins([
  
   poweredByHeader: false,
   reactStrictMode: true,
-  images: {
-    domains: ['retoolbox.netlify.app'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  i18n: {
-    locales: ['en', 'fr', 'de'],
-    defaultLocale: 'en',
-  },
-  trailingSlash: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Modify the config as needed
-    return config;
-  },
+  
 });
